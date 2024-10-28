0fafb783b521bb57a2b97ffe45804f6df5f5b3a6
AnnLinnden <annlinnden@gmail.com>
2024-10-28 20:07:23 +0600

    Готовы SQL-модели

--- database/sql_models.py
+++ database/sql_models.py
@@ -1,19 +1,18 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from fastapi import Depends
 from sqlmodel import Field, Session, SQLModel, create_engine
-from datetime import datetime
 import config
 
 
 # Данные об организации. Информации о типах отходов здесь нет, потому что они будут в post-запросах
-class Organizations(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class Organization(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)  # id будет присваиваться автоматически
     name: str = Field(default=..., description="Название организации")
 
 
 # Данные о хранилищах, включая актуальные лимиты по каждому типу отходов
-class Warehouses(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class Warehouse(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(default=..., description="Название хранилища")
     bio_limit: int = Field(default=...)
     plastic_limit: int = Field(default=...)
@@ -21,10 +20,10 @@ class Warehouses(SQLModel, table=True):
 
 
 # Хранилища, доступные для конкретных организаций, расстояние между организациями и хранилищами
-class WarehousesAvailability(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    org_id: int = Field(default=..., index=True, foreign_key="organizations.id")
-    warehouse_id: int = Field(default=..., index=True, foreign_key="warehouses.id")
+class WarehouseAvailability(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    org_id: int = Field(default=..., index=True, foreign_key="organization.id")
+    warehouse_id: int = Field(default=..., index=True, foreign_key="warehouse.id")
     dist: int = Field(default=...)
 
 
@@ -33,12 +32,11 @@ class WarehousesAvailability(SQLModel, table=True):
 # Можно добавить функцию проверки для аналитики: сколько доставок отменили в конкретный период,
 # какая организация делает это чаще всего.
 class Reservation(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    from_org: int = Field(default=..., foreign_key="organizations.id")
-    to_warehouse: int = Field(default=..., foreign_key="warehouses.id")
+    id: Optional[int] = Field(default=None, primary_key=True)
+    from_org: int = Field(default=..., foreign_key="organization.id")
+    to_warehouse: int = Field(default=..., foreign_key="warehouse.id")
     waste_type: str = Field(default=..., description='Укажите glass, plastic или bio')
     quantity: int = Field(default=...)
-    date: datetime.today() = Field(default=...)
     accepted: bool = Field()
 
 
